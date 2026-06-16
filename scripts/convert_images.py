330d6870fa458ab7f8e65bea5013990fd8ac2932
Domas Leščinskas <domas.lescinskas@gmail.com>
2026-06-16 18:15:49 +0300

    Fixed the colors so they are accurate to the real Sprig Changed the "not found color" symbol to be "." instead of "0" in the generated character maps

--- scripts/convert_images.py
+++ scripts/convert_images.py
@@ -3,22 +3,22 @@ from PIL import Image
 
 
 COLOR_MAP = {
-    (0, 0, 0, 255): '0',       # Black
-    (100, 100, 100, 255): 'L', # Dark Gray
-    (159, 159, 159, 255): '1', # Light Gray
-    (255, 255, 255, 255): '2', # White
-    (223, 23, 23, 255): '3',   # Red
-    (106, 72, 39, 255): 'C',   # Brown
-    (39, 133, 206, 255): '7',  # Light Blue
-    (31, 48, 184, 255): '5',   # Dark Blue
-    (206, 205, 23, 255): '6',  # Yellow
-    (168, 157, 23, 255): 'F',  # Gold
-    (65, 176, 48, 255): '4',   # Light Green
-    (42, 125, 24, 255): 'D',   # Dark Green
-    (239, 115, 187, 255): '8', # Pink
-    (178, 28, 173, 255): 'H',  # Magenta
-    (221, 131, 17, 255): '9',  # Orange
-    (0, 0, 0, 0): '.',         # Transparent
+    (0, 0, 0, 255): '0',        # Black
+    (74, 80, 87, 255): 'L',     # Dark Gray
+    (145, 151, 156, 255): '1',  # Light Gray
+    (248, 249, 250, 255): '2',  # White
+    (230, 49, 73, 255): '3',    # Red
+    (136, 66, 47, 255): 'C',    # Brown
+    (62, 174, 247, 255): '7',   # Light Blue
+    (50, 0, 223, 255): '5',     # Dark Blue
+    (250, 232, 31, 255): '6',   # Yellow
+    (147, 141, 52, 255): 'F',   # Gold
+    (53, 226, 64, 255): '4',    # Light Green
+    (33, 149, 19, 255): 'D',    # Dark Green
+    (242, 109, 187, 255): '8',  # Pink
+    (170, 52, 196, 255): 'H',   # Purple
+    (240, 116, 31, 255): '9',   # Orange
+    (0, 0, 0, 0): '.',          # Transparent
 }
 
 
@@ -37,7 +37,7 @@ def convert_to_raw_pixel_map(image_path):
         f.write(f"const {file_stem.upper()} = [\n")
 
         for y in range(height):
-            row_str = "".join(COLOR_MAP.get(img.getpixel((x, y)), "0") for x in range(width))
+            row_str = "".join(COLOR_MAP.get(img.getpixel((x, y)), ".") for x in range(width))
             comma = "" if y == height - 1 else ","
 
             f.write(f'  "{row_str}"{comma}\n')
